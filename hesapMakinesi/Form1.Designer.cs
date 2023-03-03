f122080daac816101219aeb389891aa69ea4e9d8
Özlem <104447793+ozlmont@users.noreply.github.com>
2023-03-03 20:53:45 +0300

    Add files via upload

---
+++ hesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace hesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(12, 28);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 16);
+            this.lblSonuc.TabIndex = 0;
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.BackColor = System.Drawing.SystemColors.Highlight;
+            this.txtSonuc.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSonuc.Location = new System.Drawing.Point(12, 47);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(447, 33);
+            this.txtSonuc.TabIndex = 1;
+            this.txtSonuc.Text = "0";
+            this.txtSonuc.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "7";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(106, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 71);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "8";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button3.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(199, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 71);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "9";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button4.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(293, 88);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 71);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.islemler);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button5.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(385, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 71);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "CE";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button6.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(12, 179);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 71);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button7.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(106, 179);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(74, 71);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button8.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(199, 179);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(74, 71);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "6";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button9.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(293, 179);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(74, 71);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "*";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.islemler);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button10.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(385, 179);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(74, 71);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "C";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button11.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(12, 270);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(74, 71);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "1";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button12.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(106, 270);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(74, 71);
+            this.button12.TabIndex = 2;
+            this.button12.Text = "2";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button13.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(199, 270);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(74, 71);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "3";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button14.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(293, 270);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(74, 71);
+            this.button14.TabIndex = 2;
+            this.button14.Text = "-";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.islemler);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button16.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.Location = new System.Drawing.Point(12, 367);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(168, 71);
+            this.button16.TabIndex = 2;
+            this.button16.Text = "0";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.Rakamlar);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button18.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button18.Location = new System.Drawing.Point(199, 367);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(74, 71);
+            this.button18.TabIndex = 2;
+            this.button18.Text = ".";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button19.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button19.Location = new System.Drawing.Point(293, 367);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(74, 71);
+            this.button19.TabIndex = 2;
+            this.button19.Text = "+";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.islemler);
+            // 
+            // button20
+            // 
+            this.button20.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button20.Font = new System.Drawing.Font("Bookman Old Style", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button20.Location = new System.Drawing.Point(385, 270);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(74, 168);
+            this.button20.TabIndex = 2;
+            this.button20.Text = "=";
+            this.button20.UseVisualStyleBackColor = false;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 494);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.lblSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+    }
+}
+
---
+++ hesapMakinesi/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        bool islemDurum=false;
+        double sonuc = 0;
+        string islem = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rakamlar(object sender, EventArgs e)
+        {
+            if (txtSonuc.Text == "0" || islemDurum)
+                txtSonuc.Clear();
+
+            islemDurum = false;
+
+            Button button = (Button)sender;
+            /* Sender parametresi: Click() fonksiyonu içerisinde yer alıyor.
+             * Kullanıcı sürekli bir nesneye yada objeye tıkladığında işlem yapıyor.
+             * Her buton için ayrı ayrı kod yazmamak için Click olayında bu işlemi yapıyoruz.
+             */
+            txtSonuc.Text += button.Text;// Alınan değeri txtSonuc ekranına yazdırıyor.
+        }
+
+        private void islemler(object sender, EventArgs e)
+        {
+            islemDurum = true;
+            Button button = (Button)sender; // Hangi butona basıldıysa o butonun basılan değerini almak için kullanıyoruz.
+            string yeniIslem = button.Text;//Eski islem bilgisini tutmak için.Eski değerini yeni değerine aktarmak için kullanıyoruz.
+                                           //Yani yeni islem bilgisini tutmak için yapıyoruz.(Bu işlem bir önceki islemi gerçekleştiriyor.)
+
+            lblSonuc.Text = lblSonuc.Text + " " + txtSonuc.Text + " " + yeniIslem;
+
+            switch (islem)
+            {
+                case "+": txtSonuc.Text = (sonuc + Double.Parse(txtSonuc.Text)).ToString(); break;
+                case "-": txtSonuc.Text = (sonuc - Double.Parse(txtSonuc.Text)).ToString(); break;
+                case "*": txtSonuc.Text = (sonuc * Double.Parse(txtSonuc.Text)).ToString(); break;
+                case "/": txtSonuc.Text = (sonuc / Double.Parse(txtSonuc.Text)).ToString(); break;
+            }
+
+            sonuc = double.Parse(txtSonuc.Text);
+            txtSonuc.Text = sonuc.ToString();
+            islem = yeniIslem;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtSonuc.Text = "0";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtSonuc.Text = "0";
+            lblSonuc.Text = "";
+            islem = "";
+            sonuc = 0;
+            
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            lblSonuc.Text = "";//İşlem sonucunun labela yansıtılması.
+            islemDurum = true;
+
+            switch (islem)
+            {
+                case "+": txtSonuc.Text = (sonuc + Double.Parse(txtSonuc.Text)).ToString(); break;
+                case "-": txtSonuc.Text = (sonuc - Double.Parse(txtSonuc.Text)).ToString(); break;
+                case "*": txtSonuc.Text = (sonuc * Double.Parse(txtSonuc.Text)).ToString(); break;
+                case "/": txtSonuc.Text = (sonuc / Double.Parse(txtSonuc.Text)).ToString(); break;
+            }
+
+            sonuc = double.Parse(txtSonuc.Text);
+            txtSonuc.Text = sonuc.ToString();
+            islem = ""; // Hafızadaki bilgiyi temizliyor.
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (txtSonuc.Text == "0")//İlk başta sıfır varsa
+            {
+                txtSonuc.Text = "0";
+            }
+            else if (islemDurum)// Herhangi bir işlem yapılmışmı onun kontrolü yapıyoruz.Amaç tekrar tekrar sıfıra basmayı engellemek.
+
+            {
+                txtSonuc.Text = "0";
+            }
+
+            if (!txtSonuc.Text.Contains(","))
+            {
+                txtSonuc.Text += ",";
+            }
+            islemDurum = false;
+        }
+    }
+}
